0188ab4fe5aa4bd197a4146cab8a8c4a91699ae1
guangyw <wanggyy@gmail.com>
2015-07-19 00:57:31 -0400

    Add threesum closest to threesum.js

--- ThreeSum.js
+++ ThreeSum.js
@@ -29,6 +29,36 @@ var threeSum = function(nums) {
     
 };
 
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+ 
+//Given an array S of n integers, are there elements a, b, c in S such that a + b + c = 0? Find all unique triplets in the array which gives the sum of zero.
+var threeSumClosest = function(nums, target) {
+    nums = nums.sort(function(a, b) {return a - b; });
+    var minDiff = Infinity;
+    var closest;
+    for (var i = 0; i < nums.length -2; i++) {
+        var middle = target - nums[i];
+        for (var m = i + 1, n = nums.length -1; m < n;) {
+            var low = nums[m], high = nums[n];
+            if (low + high === middle) return target;
+            if (Math.abs(low + high - middle) < minDiff) {
+                closest = low + high + nums[i];
+                minDiff = Math.abs(low + high - middle);
+            } 
+            if (low + high > middle) {
+                while(n > 0 && nums[n] === nums[--n]) {}
+            } else {
+                while(m < nums.length -1 && nums[m] === nums[++m]) {} 
+            }
+        }
+    }
+    return closest;
+};
+
 function incLow(nums, low) {
     while (low < nums.length -1 && nums[low] === nums[++low]) {}
     return low;
@@ -39,4 +69,7 @@ function decHigh(nums, high) {
     return high;
 }
 
-module.exports = threeSum;
+module.exports = {
+    threeSum: threeSum,
+    threeSumClosest: threeSumClosest
+};
